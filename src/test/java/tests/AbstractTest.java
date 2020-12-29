5835560fa44ec8b9b324ca1d62fd9a7735e447a2
Bivis-s <typoborov@yandex.by>
2020-12-29 22:31:48 +0300

    Added testLesson9.xml

--- src/test/java/tests/AbstractTest.java
+++ src/test/java/tests/AbstractTest.java
@@ -4,10 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import tools.TestTools;
 
 import java.io.File;
 import java.util.HashMap;
@@ -25,7 +23,7 @@ public abstract class AbstractTest {
         WebDriverManager.chromedriver().driverVersion("87.0.4280.88").setup();
 
         // Create Chrome Prefs (set download dir)
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
 //        chromePrefs.put("profile.default_content_settings.popups", 0);
         chromePrefs.put("download.default_directory", new File("src/test/resources/test_res/download").getAbsolutePath());
 
--- src/test/java/tests/dynamic_controls_test/Values.java
+++ src/test/java/tests/dynamic_controls_test/Values.java
@@ -1,8 +1,6 @@
 package tests.dynamic_controls_test;
 
 public class Values {
-    public static final int TIME_OUT_IN_SECONDS = 6;
-
     public static final String CHECKBOX_CONDITION_MESSAGE_BACK = "It's back!";
     public static final String CHECKBOX_CONDITION_MESSAGE_GONE = "It's gone!";
 
