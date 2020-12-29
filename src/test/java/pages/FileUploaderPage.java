bc9b399fa329becb0c3ccfa2e4cee4dbc139c2d7
Bivis-s <typoborov@yandex.by>
2020-12-29 18:24:09 +0300

    Added IFrameTest

--- src/test/java/pages/FileUploaderPage.java
+++ src/test/java/pages/FileUploaderPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import java.io.File;
 
 public class FileUploaderPage extends AbstractPage {
-    protected static final String FILE_UPLOADER = URL + "upload";
+    protected static final String FILE_UPLOADER_URL = URL + "upload";
     private final By INPUT_FILE_UPLOAD = By.id("file-upload");
     private final By BUTTON_SUBMIT_UPLOAD = By.id("file-submit");
     private final By DIV_UPLOADED_FILES = By.id("uploaded-files");
@@ -17,7 +17,7 @@ public class FileUploaderPage extends AbstractPage {
 
     @Override
     public void openPage() {
-        driver.get(FILE_UPLOADER);
+        driver.get(FILE_UPLOADER_URL);
     }
 
     public void uploadFileToInput(File file) {
---
+++ src/test/java/pages/IFramePage.java
@@ -0,0 +1,40 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class IFramePage extends AbstractPage{
+    protected static String IFRAME_URL = URL + "iframe";
+    private final By IFRAME = By.xpath("//iframe[contains(@class,'iframe')]");
+    private final By IFRAME_EDITOR_TEXT = By.xpath("//*[@id='tinymce']/p");
+
+    public IFramePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public void openPage() {
+        driver.get(IFRAME_URL);
+    }
+
+    public void switchToIFrame() {
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(IFRAME));
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame(driver.findElement(IFRAME));
+    }
+
+    public String getIFrameEditorText() {
+        return driver.findElement(IFRAME_EDITOR_TEXT).getText();
+    }
+
+    public void clearIFrameEditor() {
+        driver.findElement(IFRAME_EDITOR_TEXT).clear();
+    }
+
+    public void sendKeysToIFrameEditor(String... keys) {
+        driver.findElement(IFRAME_EDITOR_TEXT).sendKeys(keys);
+    }
+}
